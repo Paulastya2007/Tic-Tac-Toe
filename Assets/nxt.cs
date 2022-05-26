f0873b070bdbc3a99d6721a4e63fd4065913421c
Paulastya2007 <rajachakraborty2007@gmail.com>
2022-05-26 23:44:02 +0530

    easy ai fixed

--- Assets/nxt.cs
+++ Assets/nxt.cs
@@ -5,18 +5,13 @@ using UnityEngine.SceneManagement;
 public class nxt : MonoBehaviour
 { public Button []button=new Button[3*3];
     public Text[] txt = new Text[10];
-    private string[] giver = new string[10];
-    private bool l=false,chance = false;  //false for human player and true for easy AI
-    private sbyte y = 0;
-    public Text winer;
-    public GameObject boad,turncanger;
+    private string[] giver = { "A", "B", "c","D","e","f","g","h","i","j" };
+    private bool l=false,chance = false;
+    private sbyte y = 0;public Text winer;
+       public GameObject boad,turncanger;
         void Start()
     {
-        char m = 'a';
-        for (int i = 1; i < giver.Length; i++)
-        {
-            giver[i] = giver[i] + m; m++;
-        }
+        Time.timeScale = 1;
 
         boad.SetActive(false);turncanger.SetActive(chance);
     }
@@ -25,6 +20,46 @@ public class nxt : MonoBehaviour
     }
         void Update()
     {
+        if (Time.timeScale != 0)
+        {
+            if (y != 9)
+            {
+                if (chance)
+                {
+                    turncanger.SetActive(true);
+
+                    while (chance)
+                    {
+                        int m = chan();//get random nmber only once
+                        if (giver[m].Equals("X") || giver[m].Equals("O"))
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            if (m == 0) { r1c1b(); }
+                            else if (m == 1) { r1c2b(); }
+                            else if (m == 2) { r1c3b(); }
+                            else if (m == 3) { r2c1b(); }
+                            else if (m == 4) { r2c2b(); }
+                            else if (m == 5) { r2c3b(); }
+                            else if (m == 6) { r3c1b(); }
+                            else if (m == 7) { r3c2b(); } else if (m == 8) { r3c3b(); }
+                            chance = false; break;
+                        }
+                    }
+
+                }
+                else { turncanger.SetActive(false); }
+            }
+        }
+    }
+    static int chan() {
+        return Random.Range(0, 9);
+    }
+    void LateUpdate()
+    {
+        txt[9].text = check(y);
         if (giver[0].Equals(giver[3]) && giver[3].Equals(giver[6])) { enablerd(giver[0]); }
         else if (giver[4].Equals(giver[1]) && giver[4].Equals(giver[7])) { enablerd(giver[7]); }
         else if (giver[2].Equals(giver[5]) && giver[5].Equals(giver[8])) { enablerd(giver[8]); }
@@ -33,47 +68,13 @@ public class nxt : MonoBehaviour
         else if (giver[8].Equals(giver[7]) && giver[7].Equals(giver[6])) { enablerd(giver[6]); }
         else if (giver[0].Equals(giver[4]) && giver[4].Equals(giver[4 * 2])) { enablerd(giver[4 * 2]); }
         else if (giver[2].Equals(giver[4]) && giver[2 * 2].Equals(giver[2 * 3])) { enablerd(giver[3 * 2]); }
-        else if (y == 10)
+        else if (y == 9)
         {
             enablerd(" Draw! ");
             l = true;
         }
-       
-        
-    }static int chan() {
-        return Random.Range(0, 9);
-    }
-    void LateUpdate() {txt[9].text = check(y);
-        if (Time.timeScale != 0)
-        {
-            if (chance)
-            {
-                turncanger.SetActive(true);
 
-                while (chance)
-                {
-                    int m = chan();//get random nmber only once
-                    if (giver[m].Equals("X") || giver[m].Equals("O"))
-                    {
-                        continue;
-                    }
-                    else
-                    {
-                        if (m == 0) { r1c1b(); }
-                        else if (m == 1) { r1c2b(); }
-                        else if (m == 2) { r1c3b(); }
-                        else if (m == 3) { r2c1b(); }
-                        else if (m == 4) { r2c2b(); }
-                        else if (m == 5) { r2c3b(); }
-                        else if (m == 6) { r3c1b(); }
-                        else if (m == 7) { r3c2b(); } else if (m == 8) { r3c3b(); }
-                        chance = false; break;
-                    }
-                }
 
-            }
-            else { turncanger.SetActive(false); }
-        }
     }
     public void r1c1b()
     {
