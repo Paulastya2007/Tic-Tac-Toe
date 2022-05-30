4ca8997776a02581dd8a176a61b9cea2ddb1b108
Paulastya2007 <rajachakraborty2007@gmail.com>
2022-05-30 14:49:40 +0530

    main menu

---
+++ Assets/menu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class menu : MonoBehaviour
+{public void easyAI() {
+        SceneManager.LoadScene("ai");
+    }
+    public void LocalMl()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+    public void qit() {
+        Application.Quit();
+    }
+    public void rettomen() {
+        SceneManager.LoadScene("unt");
+    }
+}
